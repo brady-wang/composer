3b5e4bd7d450eb96ee307ad48683f20f19aa11c4
brady <brady.wang@qq.com>
2019-03-31 11:27:29 +0800

    sdf

--- index.php
+++ index.php
@@ -11,4 +11,26 @@ spl_autoload_register("\Core\Loader"."::loadClass");
 
 
 require BASE_PATH."/vendor/autoload.php";
-call_user_func("\Core\Run::init");
+
+dump("dddd"); //使用公共函数'
+$db = new \Libraries\Db(); //使用db类 已经通过composer引入了
+echo $db->getInstance();
+
+$redis = new \Libraries\Redis();//已经通过composer 的classmap引入了
+$redis->getInstance();
+
+
+echo "<hr>";//自动加载本项目的类 通过psr
+use Wang\Test;
+
+$a = new Test();
+$a->tt();
+
+echo "<hr>";
+//通过psr加载第三方包
+use Brady\Tool;
+
+$db = new Tool\Db\DB();
+
+$db->hello();
+Tool\Redis::getInstance();
